9e00c84580cc6230d48827637a2067f614aca012
Egor Shitikov <egorshitikov@gmail.com>
2023-11-07 11:05:19 -0800

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,4 +4,5 @@ sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.g
 echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
 sudo apt-get update
 sudo apt-get install k6
+sudo apt-get install wget
 wget https://raw.githubusercontent.com/Genaker/magento-k6-performance/main/magento.js
